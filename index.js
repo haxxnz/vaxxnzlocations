68c58b35f6e4f41005b8094ec6d20b47a874bd84
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-08-31 19:53:01 +1200

    recursively get

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fetch = require("node-fetch");
-async function getLocations() {
+async function getLocations(cursor) {
   const res = await fetch(
     "https://skl-api.bookmyvaccine.covid19.health.nz/public/locations/search",
     {
@@ -10,20 +10,20 @@ async function getLocations() {
       body: JSON.stringify({
         // location: { lat: -36.8560865, lng: 174.8232806 },
         location: { lat: -36.8534194, lng: 174.7595025 },
-        
-        // lng: 
+
+        // lng:
         fromDate: "2021-08-31",
         vaccineData: "WyJhMVQ0YTAwMDAwMEhJS0NFQTQiXQ==",
         locationQuery: {
           includePools: ["default"],
           includeTags: [],
-        //   excludeTags: ["a0p4a000000CInuAAG"],
+          //   excludeTags: ["a0p4a000000CInuAAG"],
           excludeTags: [],
         },
         doseNumber: 1,
         groupSize: 1,
         limit: 20,
-        cursor: "",
+        cursor: cursor,
         locationType: "CombinedBooking",
         filterTags: [],
         url: "https://app.bookmyvaccine.covid19.health.nz/location-select",
@@ -32,7 +32,12 @@ async function getLocations() {
     }
   );
   const data = await res.json();
+  const newCursor = data.cursor;
+  console.log(newCursor);
   console.log("data", data.locations.length);
+  if (newCursor) {
+    return getLocations(newCursor);
+  }
 }
 
 getLocations();
