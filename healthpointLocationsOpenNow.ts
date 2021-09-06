72a4a19e6fbd4d30af1254b98c06ef229dc86579
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2021-09-06 20:56:43 +1200

    add website

--- healthpointLocationsOpenNow.ts
+++ healthpointLocationsOpenNow.ts
@@ -62,10 +62,10 @@ function nameAddressCommunity($: CheerioAPI) {
 
 }
 
-async function getHealthpointLocation(body: string, url: string, branch: Branch) {
+async function getHealthpointLocation(body: string, website: string, branch: Branch) {
   const $ = cheerio.load(body);
   const {name, address} = branch === "community" ? nameAddressCommunity($) : nameAddressNormal($)
-  console.log('--> url',url)
+  console.log('--> url',website)
   console.log({branch,name,address})
   // console.log('branch',branch)
   // console.log('name',name)
@@ -152,6 +152,7 @@ async function getHealthpointLocation(body: string, url: string, branch: Branch)
     address,
     telephone,
     faxNumber,
+    website,
     opennningHours,
   }
 
