c39e7637ec6a6a76cc3b6765d1ea771cf9c4abef
Ramon Quitales <me@rquitales.com>
2021-09-14 15:56:17 +1200

    fix: fix parsing errors for public holiday
    Some public holiday entries for locations contain times. When we split strings with colon,
    we need to take this into account so that information is not prematurely cut off.
    
    Signed-off-by: Ramon Quitales <me@rquitales.com>

--- healthpointLocationsOpenNow.ts
+++ healthpointLocationsOpenNow.ts
@@ -139,7 +139,7 @@ async function getHealthpointLocation(body: string, url: string, branch: Branch)
     const holidayHoursTexts = holidayHoursEls.map((i, el) => $(el).text()).get();
     let exceptions: Record<string, string> = {}
     holidayHoursTexts.map(holidayHoursText => {
-      const [key, value] = holidayHoursText.split(':')
+      const [key, value] = holidayHoursText.split(': ')
       exceptions[key] = value
     })
 
